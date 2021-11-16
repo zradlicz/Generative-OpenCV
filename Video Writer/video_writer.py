75d336e288f8df7cfdcd27e3fb63326f59d7f93a
Zach Radlicz <66800917+zradlicz@users.noreply.github.com>
2021-11-16 08:09:25 -0600

    Update video_writer.py

--- Video Writer/video_writer.py
+++ Video Writer/video_writer.py
@@ -7,9 +7,12 @@ Created on Fri Jun  4 15:01:14 2021
 import cv2
 import numpy as np
 import glob
- 
+
+FOLDER_PATH = 'C:/Users/zradlicz/Desktop/Misc Art/raytracing/cloud_images'
+VIDEO_NAME = 'clouds'
+
 img_array = []
-for filename in sorted(glob.glob('C:/Users/zradlicz/Desktop/Misc Art/raytracing/cloud_images/*.jpg')):
+for filename in sorted(glob.glob(FOLDER_PATH+'/*.png')):
     img = cv2.imread(filename)
     print(filename)
     height, width, layers = img.shape
@@ -17,7 +20,7 @@ for filename in sorted(glob.glob('C:/Users/zradlicz/Desktop/Misc Art/raytracing/
     img_array.append(img)
  
  
-out = cv2.VideoWriter('clouds.avi',cv2.VideoWriter_fourcc(*'DIVX'), 10, size)
+out = cv2.VideoWriter(VIDEO_NAME+'.avi',cv2.VideoWriter_fourcc(*'DIVX'), 10, size)
  
 for i in range(len(img_array)):
     out.write(img_array[i])
